5e01e94bc29f08a9df73c97d32451a3aa16b7aaa
Mauricio <mauba22@outlook.com>
2024-10-09 12:41:06 -0600

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,44 @@
-
-/* Rotate */
-const rotateX = document.getElementById('rotx'), rotateY = document.getElementById('roty'), 
-    rotateZ = document.getElementById('rotz')
-/* Scale */
-const scaleX = document.getElementById('scax'), scaXValue = document.getElementById('scaxRange'),
-    scaleY = document.getElementById('scay'), scaYValue = document.getElementById('scayRange')
-/* Skew */
+const rotateX = document.getElementById('rotx'), rotateY = document.getElementById('roty'), rotateZ = document.getElementById('rotz')
+const scaleX = document.getElementById('scax'), scaXValue = document.getElementById('scaxRange'), scaleY = document.getElementById('scay'), scaYValue = document.getElementById('scayRange')
 const skewX = document.getElementById('skx'), skewY = document.getElementById('sky')
-/* Translate */
-const tranX = document.getElementById('tranx'), tranY = document.getElementById('trany'), 
-    tranZ = document.getElementById('tranz')
-const transformPreview = document.querySelector('.preview'), cssCodeText = document.getElementById('CSS-code'),
-    resetBtn = document.getElementById('reset-btn'), copyCode = document.querySelector('.copy-btn')
+const tranX = document.getElementById('tranx'), tranY = document.getElementById('trany'), tranZ = document.getElementById('tranz')
+const transformPreview = document.querySelector('.preview'), cssCodeText = document.getElementById('CSS-code'), resetBtn = document.getElementById('reset-btn'), copyCode = document.querySelector('.copy-btn')
 
 window.onload = () => {
     resetElements()
-    generateTransformStyles()
-    cssCodeText.value = ''
 }
 
 const propertiesColumns = document.querySelectorAll('.column input')
 propertiesColumns.forEach(element => {
     element.addEventListener('input', () => {
         generateTransformStyles()
-        transformPreview.style.opacity = '0.75'
+        transformPreview.style.opacity = '0.75'          
     })
 });
 
 const generateTransformStyles = () => {   
-    let scaleCode, translateCode, skewCode, rotX, rotY, rotZ
-    /* Scale */
+    let scaleCode, transCode, skewCode, rotX, rotY, rotZ
     scaleCode = scaleX.value > 1 ? `${scaleX.value} ` : '', scaleCode += scaleY.value > 1 ? `${scaleY.value}` : ''
     transformPreview.style.scale = scaleCode
-    /* Translate */
-    translateCode = tranX.value != 0 || tranY.value != 0 || tranZ.value != 0 ? `${tranX.value}px ${tranY.value}px ${tranZ.value}px` : ''
-    transformPreview.style.translate = translateCode
-    /* Rotate */
-    rotX = rotateX.value != 0 ? `rotateX(${rotateX.value}deg) ` : '', rotY = rotateY.value != 0 ? `rotateY(${rotateY.value}deg) ` : '', 
-        rotZ = rotateZ.value != 0 ? `rotateZ(${rotateZ.value}deg)` : ''
-    /* Skew */
-    skewCode = skewX.value != 0 ? `${skewX.value}deg` : '', skewCode += skewY.value != 0 ? skewX.value != 0 && skewY.value != 0 ?
-        `, ${skewY.value}deg` : `${skewY.value}deg` : ''
-    let code = `${rotX + rotY + rotZ} ${skewCode != '' ? `skew(${skewCode})` : ''}` 
-    transformPreview.style.transform = code
 
-    cssCodeText.value = `transform: ${code};${translateCode != '' ? `\ntranslate: ${translateCode};` : ''}${scaleCode != '' ? `\nscale: ${scaleCode};` : ''}\n\n//Pegar en el elemento padre, en caso de ser necesario:\nperspective: 200px;`
+    transCode = tranX.value != 0 || tranY.value != 0 || tranZ.value != 0 ? `${tranX.value}px ${tranY.value}px ${tranZ.value}px` : ''
+    transformPreview.style.translate = transCode
+
+    rotX = rotateX.value > 0 ? `rotateX(${rotateX.value}deg) ` : '', rotY = rotateY.value > 0 ? `rotateY(${rotateY.value}deg) ` : '', rotZ = rotateZ.value > 0 ? `rotateZ(${rotateZ.value}deg)` : ''
+
+    skewCode = skewX.value > 0 ? `${skewX.value}deg` : '', skewCode += skewY.value > 0 ? skewX.value > 0 && skewY.value > 0 ?
+        `, ${skewY.value}deg` : `${skewY.value}deg` : ''
+
+    let code = `${rotX.length > 0 ? rotX : ''} ${rotY.length > 0 ? rotY : ''} ${rotZ.length > 0 ? rotZ : ''} ${skewCode != '' ? `skew(${skewCode})` : ''}` 
+    cssCodeText.value = `${code.length > 3 ? `transform: ${code};` : ''} ${transCode != '' ? `\ntranslate: ${transCode};` : ''} ${scaleCode != '' ? `\nscale: ${scaleCode};` : ''}\n\n/* Pegar en el elemento padre */ \nperspective: 200px;`
+    
+    !code.includes('rotate') ? transformPreview.style.transform = `rotate(0deg) ${code}` : transformPreview.style.transform = code
 }       
 
 const resetElements = () => {   
     propertiesColumns.forEach(element => { element.value = 0 });
     scaleX.value = scaXValue.value = scaleY.value = scaYValue.value = 1
-    cssCodeText.value = ''
-    transformPreview.style.transform = '', transformPreview.style.opacity = '1'
+    cssCodeText.value = '', transformPreview.style.transform = '', transformPreview.style.opacity = '1', transformPreview.style.scale = '1', transformPreview.style.translate = '0'
 }
 
 resetBtn.addEventListener('click', () => {
